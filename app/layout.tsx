771223934b12f1249d0fd5f2f4fd47917e82e3b9
amilcarfilemone-dev <amilcarfilemone@gmail.com>
2026-09-03 04:29:16 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "IMPULSO",
+  description: "Não pares. Evolui.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt">
+      <body>{children}</body>
+    </html>
+  );
+}
